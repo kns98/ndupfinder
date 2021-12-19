cd9f350b2616e0081f8fd9c291c13ac47b6057b8
kevin <kevin@BIBINU-NITRO>
2021-12-19 21:53:31 +0800

    vs2022

--- src/console/Properties/Resources.Designer.cs
+++ src/console/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace deduper.wpf.Properties {
+namespace deduper.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace deduper.wpf.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("deduper.wpf.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("deduper.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
